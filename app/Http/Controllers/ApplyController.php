bbbf525ab523cf07766d88c9913af6f5d2f24af3
yaoluchao <yaoluchao@geexfinance.com>
2018-11-21 14:10:54 +0800

    fix

--- app/Http/Controllers/ApplyController.php
+++ app/Http/Controllers/ApplyController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class ApplyController extends Controller
 {
-    //
+    public function store(Request $request){
+      echo $name = $request->input('name','呵呵哒');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+//用户记录新增
+Route::post('apply/store', 'ApplyController@store');
+
